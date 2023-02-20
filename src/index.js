fef93d45de5af7a907a281da7fc416c0fdde17e3
Tiago Novais [1221866] <1221866@isep.ipp.pt>
2023-02-20 14:49:25 +0000

    to redux changess

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import App from "./App";
-import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 import "./index.scss";
 
@@ -15,24 +14,15 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
-
-
-
-
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,14 +1,31 @@
-import { Routes, Route } from "react-router-dom";
-import CategoriesPreview from "../categories-preview/categories-preview.component";
-import Category from "../category/category.component";
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+import { setCategories } from '../../store/categories/categories.action';
+
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 const Shop = () => {
-  return (
-      <Routes>
-        <Route index element={<CategoriesPreview />} />
-        <Route path=":category" element={<Category />} />
-      </Routes>
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categories = await getCategoriesAndDocuments('categories');
+      dispatch(setCategories(categories));
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
   );
 };
 
